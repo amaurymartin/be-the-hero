e19f08a08e1f4d969a1a27858032be66da8e98f4
Amaury Martin <amaury.conde@hotmail.com>
2021-04-06 00:38:21 -0300

    Create Dashboard component

---
+++ frontend/web/src/pages/dashboard/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './style.css';
+
+export default function Dashboard() {
+  return(
+    <h1>Dashboard</h1>
+  );
+}
--- frontend/web/src/routes.js
+++ frontend/web/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './pages/login';
 import Register from './pages/register';
+import Dashboard from './pages/dashboard';
 
 export default function Routes() {
   return (
@@ -10,6 +11,7 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact component={Login} />
         <Route path="/register" component={Register} />
+        <Route path="/dashboard" component={Dashboard} />
       </Switch>
     </BrowserRouter>
   );
