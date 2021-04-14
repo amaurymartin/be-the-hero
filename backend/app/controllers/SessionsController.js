650b2aad7045f2cafc42869234a054aebb9f2d38
Amaury Martin <amaury.conde@hotmail.com>
2021-04-14 02:04:03 -0300

    Linting sessions controller

--- backend/app/controllers/SessionsController.js
+++ backend/app/controllers/SessionsController.js
@@ -1,21 +1,23 @@
-const connection = require('../../db/connection');
 const {
   v4: uuidv4,
 } = require('uuid');
+
+const connection = require('../../db/connection');
 
 module.exports = {
   async create(req, res) {
     const userEmail = req.headers.email;
     const userPassword = req.headers.password;
 
-    const user = await connection('users').select('*').where({email: userEmail, password: userPassword}).first();
+    const user = await connection('users').select('*').where({ email: userEmail, password: userPassword }).first();
     let ngo = {};
 
-    if(user)
-      ngo = await connection('ngos').select('*').where({id: user.ngo_id});
-    else
-      return res.status(401).json({error: 'Not authorized'});
+    if (user) {
+      ngo = await connection('ngos').select('*').where({ id: user.ngo_id });
+    } else {
+      return res.status(401).json({ error: 'Not authorized' });
+    }
 
     return res.json(ngo);
-  }
+  },
 };
