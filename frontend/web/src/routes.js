f8751604799be699a262b27ad7bd663271b173e8
Amaury Martin <amaury.conde@hotmail.com>
2021-04-14 00:38:52 -0300

    Fix routes

--- frontend/web/src/routes.js
+++ frontend/web/src/routes.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './pages/login';
-import Register from './pages/register';
-import Dashboard from './pages/dashboard';
-import NewIncident from './pages/incident/new';
+import NewUser from './pages/users/new';
+import ShowOrganization from './pages/organizations/show';
+import NewOrganization from './pages/organizations/new';
+import NewIncident from './pages/incidents/new';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/users/new" component={NewUser} />
+        <Route path="/organizations/key" component={ShowOrganization} />
+        <Route path="/organizations/new" component={NewOrganization} />
         <Route path="/incidents/new" component={NewIncident} />
       </Switch>
     </BrowserRouter>
