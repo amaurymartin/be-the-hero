566f52c79575bdc05dea154e3a3989c250308636
Amaury Martin <amaury.conde@hotmail.com>
2021-04-14 00:38:29 -0300

    Create ShowOrganization component

---
+++ frontend/web/src/pages/organizations/show/index.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import './style.css';
+
+import logoSvg from '../../../assets/logo.svg';
+
+export default function ShowOrganization() {
+  return(
+    <div className="show-organization-container">
+      <header>
+        <img src={logoSvg} alt="Be the Hero"/>
+        <span>Welcome!</span>
+
+        <Link className="button" to="/incidents/new">New incident</Link>
+        <button type="button">
+          <FiPower size={18} color="#e02048"></FiPower>
+        </button>
+      </header>
+
+      <h1>Incidents</h1>
+
+      <ul>
+        <li>
+          <strong>Incident:</strong>
+          <p>Incident title</p>
+
+          <strong>Description:</strong>
+          <p>Incident description</p>
+
+          <strong>Value:</strong>
+          <p>Incident value</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3">
+
+            </FiTrash2>
+          </button>
+        </li>
+
+        <li>
+          <strong>Incident:</strong>
+          <p>Incident title</p>
+
+          <strong>Description:</strong>
+          <p>Incident description</p>
+
+          <strong>Value:</strong>
+          <p>Incident value</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3">
+
+            </FiTrash2>
+          </button>
+        </li>
+        <li>
+          <strong>Incident:</strong>
+          <p>Incident title</p>
+
+          <strong>Description:</strong>
+          <p>Incident description</p>
+
+          <strong>Value:</strong>
+          <p>Incident value</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3">
+
+            </FiTrash2>
+          </button>
+        </li>
+        <li>
+          <strong>Incident:</strong>
+          <p>Incident title</p>
+
+          <strong>Description:</strong>
+          <p>Incident description</p>
+
+          <strong>Value:</strong>
+          <p>Incident value</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3">
+
+            </FiTrash2>
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
