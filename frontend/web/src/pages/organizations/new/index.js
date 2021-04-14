be3ed0c63e72d65c3f96beb1c20043aeff9dedb9
Amaury Martin <amaury.conde@hotmail.com>
2021-04-14 00:38:15 -0300

    Create NewOrganization component

---
+++ frontend/web/src/pages/organizations/new/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './style.css';
+
+import logoSvg from '../../../assets/logo.svg';
+
+export default function NewOrganization() {
+  return (
+    <div className="new-organization-container">
+      <div className="content">
+        <section>
+          <img src={logoSvg} alt="Be the Hero"/>
+
+          <h1>New Non-Governmental Organization</h1>
+          <p>Submit your Organization infos to be able to post your incidents</p>
+
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#e02041" />
+            Sign up
+          </Link>
+        </section>
+
+        <form>
+          <input placeholder="Name"/>
+          <input placeholder="Nickname"/>
+          <input type= "email" placeholder="Email"/>
+          <input type= "whatsapp" placeholder="Whatsapp"/>
+          <input type= "city" placeholder="City"/>
+          <input type= "state" placeholder="State"/>
+          <input type= "country" placeholder="Country"/>
+
+          <button className="button" type="submit">Create Organization</button>
+        </form>
+      </div>
+    </div>
+  );
+}
