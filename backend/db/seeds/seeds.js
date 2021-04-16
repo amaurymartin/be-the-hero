482923d74f4ccac7b26b4c7673d9506861491b1d
Amaury Martin <amaury.conde@hotmail.com>
2021-04-16 02:12:40 -0300

    Fix database files

--- backend/db/seeds/seeds.js
+++ backend/db/seeds/seeds.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
--- backend/knexfile.js
+++ backend/knexfile.js
@@ -4,47 +4,47 @@ module.exports = {
     client: 'postgresql',
     connection: {
       host: '127.0.0.1',
-      database: 'omnistack_11',
-      user:     'amaurymartin',
+      database: 'be-the-hero',
+      user: 'amaurymartin',
       password: 'amaurymartin',
-      charset: 'utf-8'
+      charset: 'utf-8',
     },
     migrations: {
       tableName: 'migrations',
-      directory: './db/migrations'
+      directory: './db/migrations',
     },
     seeds: {
-      directory: './db/seeds'
-    }
+      directory: './db/seeds',
+    },
   },
   staging: {
     client: 'postgresql',
     connection: {
-      database: 'omnistack_11',
-      user:     'amaurymartin',
-      password: 'amaurymartin'
+      database: 'be-the-hero',
+      user: 'amaurymartin',
+      password: 'amaurymartin',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'db_migrations'
-    }
+      tableName: 'db_migrations',
+    },
   },
   production: {
     client: 'postgresql',
     connection: {
-      database: 'omnistack_11',
-      user:     'amaurymartin',
-      password: 'amaurymartin'
+      database: 'be-the-hero',
+      user: 'amaurymartin',
+      password: 'amaurymartin',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'db_migrations'
-    }
-  }
+      tableName: 'db_migrations',
+    },
+  },
 };
