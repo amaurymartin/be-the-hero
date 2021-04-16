7ab947b258b92af107ad835e962f9046df9f4ae0
Amaury Martin <amaury.conde@hotmail.com>
2021-04-16 02:13:08 -0300

    Fix routes

--- backend/app/routes.js
+++ backend/app/routes.js
@@ -3,8 +3,7 @@ const express = require('express');
 const routes = express.Router();
 
 const SessionsController = require('./controllers/SessionsController');
-const NgosController = require('./controllers/NgosController');
-const UsersController = require('./controllers/UsersController');
+const OrganizationsController = require('./controllers/OrganizationsController');
 const IncidentsController = require('./controllers/IncidentsController');
 
 // Home
@@ -18,23 +17,14 @@ routes.get('/', (req, res) => {
 
 // Sessions resource
 routes.post('/sessions', SessionsController.create);
-// routes.delete('/sessions', SessionsController.delete);
 
-// NGOs resource
-routes.post('/ngos', NgosController.create);
-routes.get('/ngos', NgosController.index);
-routes.get('/ngos/:key', NgosController.show);
-routes.put('/ngos/:key', NgosController.update);
-// routes.patch('/ngos/:key', NgosController.patch);
-routes.delete('/ngos/:key', NgosController.delete);
-
-// Users resource
-routes.post('/users', UsersController.create);
-routes.get('/users', UsersController.index);
-routes.get('/users/:key', UsersController.show);
-routes.put('/users/:key', UsersController.update);
-// routes.patch('/users/:key', UsersController.patch);
-routes.delete('/users/:key', UsersController.delete);
+// Organizations resource
+routes.post('/organizations', OrganizationsController.create);
+routes.get('/organizations', OrganizationsController.index);
+routes.get('/organizations/:key', OrganizationsController.show);
+routes.put('/organizations/:key', OrganizationsController.update);
+// routes.patch('/organizations/:key', OrganizationsController.patch);
+routes.delete('/organizations/:key', OrganizationsController.delete);
 
 // Incidents resource
 routes.post('/incidents', IncidentsController.create);
