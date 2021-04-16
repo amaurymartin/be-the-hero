62d8ebbe076fec0b2fc5cad12d21a715ba0e3538
Amaury Martin <amaury.conde@hotmail.com>
2021-04-16 02:09:25 -0300

    Enable cors

--- backend/app/index.js
+++ backend/app/index.js
@@ -6,9 +6,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.json());
-app.use(routes);
 app.use(cors());
+app.use(routes);
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
