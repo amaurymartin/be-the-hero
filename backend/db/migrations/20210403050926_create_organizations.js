3bf8dd3e7bcd31edeae40c4cd2de956438399e5f
Amaury Martin <amaury.conde@hotmail.com>
2021-04-18 15:11:25 -0300

    Add password to organizations migration

--- backend/db/migrations/20210403050926_create_organizations.js
+++ backend/db/migrations/20210403050926_create_organizations.js
@@ -8,6 +8,7 @@ exports.up = function(knex) {
     table.string('name').notNullable();
     table.string('nickname').notNullable();
     table.string('email').notNullable();
+    table.string('password').notNullable();
     table.string('whatsapp').notNullable();
     table.string('city').notNullable();
     table.string('state').notNullable();
