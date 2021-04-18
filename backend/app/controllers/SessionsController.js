8c71903b2fe7a9f7c2a72530857a27ffa47627dc
Amaury Martin <amaury.conde@hotmail.com>
2021-04-18 17:18:12 -0300

    Fix session controller

--- backend/app/controllers/SessionsController.js
+++ backend/app/controllers/SessionsController.js
@@ -2,14 +2,13 @@ const connection = require('../../db/connection');
 
 module.exports = {
   async create(req, res) {
-    const userEmail = req.headers.email;
-    const userPassword = req.headers.password;
+    const { email, password } = req.headers;
 
     const organization = await connection('organizations').select('*')
-      .where({ email: userEmail, password: userPassword }).first();
+      .where({ email, password }).first();
 
     if (!organization) return res.status(401).json({ error: 'Not authorized' });
 
-    return res.json(organization);
+    return res.status(201).json(organization);
   },
 };
