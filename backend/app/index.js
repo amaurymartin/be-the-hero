5acce57199e32d0f8c73eae7087ec6664f144d66
Amaury Martin <amaury.conde@hotmail.com>
2021-04-18 15:31:41 -0300

    Fix express config for api

--- backend/app/index.js
+++ backend/app/index.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(cors());
-app.use(routes);
 app.use(express.json());
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
