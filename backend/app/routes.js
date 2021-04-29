a7acd749edb865b7e74558b66fd26067e4360ed4
Amaury Martin <amaury.conde@hotmail.com>
2021-04-29 00:41:17 -0300

    Add users routes

--- backend/app/routes.js
+++ backend/app/routes.js
@@ -14,6 +14,14 @@ routes.get('/healthcheck', (_req, res) => {
 // Sessions resource
 routes.post('/sessions', SessionsController.create);
 
+// Users resource
+// routes.post('/users', UsersController.create);
+// routes.get('/users', UsersController.index);
+// routes.get('/users/:key', UsersController.show);
+// routes.put('/users/:key', UsersController.update);
+// // routes.patch('/users/:key', UsersController.patch);
+// routes.delete('/users/:key', UsersController.delete);
+
 // Organizations resource
 routes.post('/organizations', OrganizationsController.create);
 routes.get('/organizations', OrganizationsController.index);
